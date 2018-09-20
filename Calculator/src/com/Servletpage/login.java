b2c1dc3ee28426bd92a035c471edc5fd15582482
Thiyagu - makpo <Thiyagu - makpo@192.168.1.7>
2018-09-20 13:03:22 +0530

    finally commited ma

---
+++ Calculator/src/com/Servletpage/login.java
@@ -0,0 +1,73 @@
+package com.Servletpage;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.user.user;
+import com.userDao.userDao;
+
+/**
+ * Servlet implementation class ServerAction
+ */
+@WebServlet("/login")
+public class login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		// TODO Auto-generated method stub
+		
+		try
+		{	    
+
+		      user e = new user();
+		     e.setEmail(request.getParameter("email"));
+		     e.setPass(request.getParameter("pass"));
+
+		     e = userDao.login(e);
+			   		    
+		     if (e.isValid())
+		     {
+			        
+		          HttpSession session = request.getSession(true);	    
+		          session.setAttribute("currentSessionUser",e); 
+		          response.sendRedirect("Homepage.jsp"); //logged-in page      		
+		     }
+			        
+		     else 
+		          response.sendRedirect("invalidLogin.jsp"); //error page 
+		} 
+				
+				
+		catch (Throwable theException) 	    
+		{
+		     System.out.println(theException); 
+		}
+		       }
+			}
